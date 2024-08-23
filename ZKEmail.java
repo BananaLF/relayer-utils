e21de25d9107def184b4593872988c9ac04afe15
fei.li <fei.li@okg.com>
2024-08-23 18:52:25 +0800

    delete eml file

--- ZKEmail.java
+++ ZKEmail.java
@@ -12,7 +12,7 @@ class ZKEmail {
     public static void main(String[] args) {
         
         try {
-        String rawEmail = new String(Files.readAllBytes(Paths.get("/Users/oker/workspace-go-build/zk-email/relayer-utils/okx_pay_test0.eml")));
+        String rawEmail = new String(Files.readAllBytes(Paths.get("./okx_pay_test0.eml")));
         String output = ZKEmail.generateEmailInput(rawEmail,"0x01eb9b204cc24c3baee11accc37d253a9c53e92b1a2cc07763475c135d575b76");
         System.out.println(output);
         } catch (IOException e) {
