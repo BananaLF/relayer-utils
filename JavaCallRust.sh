3c2bc9a3fc28aa94d0971c5324ae67fad0adff63
fei.li <fei.li@okg.com>
2024-09-02 22:36:05 +0800

    add java lib

--- JavaCallRust.sh
+++ JavaCallRust.sh
@@ -1,8 +1,14 @@
-#1. complie rust link 
-cargo build
+#1. complie rust link
+
+#if you got some link error related platform,
+# you can custom build yourself lib.
+cargo build --release
+# macos suffix is dylib
+# linux suffix is so
+cp ./target/release/librelayer_utils.dylib ./javalib
 
 #2. complie java
 javac ZKEmail.java
 
 #3. run java
-java -Djava.library.path=./target/debug ZKEmail
+java -Djava.library.path=./javalib ZKEmail
